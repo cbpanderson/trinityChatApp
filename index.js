3ccd6419b5758a34a73a72e444528a7e69d33181
Cassie Anderson <cbp.smith@gmail.com>
2021-06-17 20:45:14 -0400

    tutorial completed, app working, no modifications yet

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
     console.log('a user connected');
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+      });
   });
+
 
 server.listen(3000, () => {
     console.log('listening on *:3000');
